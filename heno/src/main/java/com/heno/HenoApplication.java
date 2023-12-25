636ede201e64211ca31dda2c2bf6d0f7cd9feca8
kostya1205 <kostya.zhurovich12052@gmail.com>
2023-12-25 19:51:42 +0300

    Create project

---
+++ heno/src/main/java/com/heno/HenoApplication.java
@@ -0,0 +1,13 @@
+package com.heno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenoApplication.class, args);
+	}
+
+}
---
+++ heno/src/test/java/com/heno/HenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
